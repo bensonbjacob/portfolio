2eed7b2ae9567530e2910a3430c52afa1b2a0551
Jacob Benson <benson.b.jacob@gmail.com>
2022-10-08 18:34:16 -0500

    Added main section component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   AiOutlineClose,
   AiOutlineMail,
