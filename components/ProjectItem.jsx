218672befc8bd837149142745c8fc3a006dafc2e
Jacob Benson <benson.b.jacob@gmail.com>
2022-10-20 09:13:15 -0500

    Update projectitem page

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const ProjectItem = ({ title, backgroundImg, projectUrl }) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#4f6d7a] to-[#140f2d]'>
       <Image
         className='rounded-xl group-hover:opacity-10'
         src={backgroundImg}
