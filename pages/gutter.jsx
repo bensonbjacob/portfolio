72f73140ec442958f9b1b3c011d79ac78355b31d
Jacob Benson <benson.b.jacob@gmail.com>
2022-10-25 11:53:57 -0500

    Update gutter calculator project page

--- pages/gutter.jsx
+++ pages/gutter.jsx
@@ -18,7 +18,7 @@ const gutter = () => {
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
           <h2 className='py-2'>Gutter Estimate Calculator</h2>
-          <h3>React</h3>
+          <h3>NextJS</h3>
         </div>
       </div>
       <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
